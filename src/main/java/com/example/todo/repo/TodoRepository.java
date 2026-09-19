83870b912210ffb5b5298f58d8677333681bef6d
KRIETHIKESH R M <132364288+kriethikesh007@users.noreply.github.com>
2026-09-19 10:23:39 +0530

    Delete src/main/java/com/example/todo directory

--- src/main/java/com/example/todo/repo/TodoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.todo.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.todo.model.Todomodel;
-
-public interface TodoRepository extends JpaRepository<Todomodel,Long>{
-
-}
